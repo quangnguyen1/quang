5864f2be8f24bc488837bef231266e1f0460d162
quangnguyen1 <quangnguyen201196@gmail.com>
2018-04-09 20:57:00 +0700

    my commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 		   flash[:success] = "Welcome to the Sample App!"
+		   #log_in @user
 		redirect_to @user
 		else
 			render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
-# Rails.application.routes.draw do
-# 	root "static_pages#home"#xu ly trang home khi can hien thi ngay localhost:3000
-#   get 'pages/privacy'
-
-#   get 'pages/inctroduction'
-
-# 	get "static_pages/home"
-# 	get "static_pages/help"
-# 	get "static_pages/about"
-#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-# end
 
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help', as: 'helf'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   resources :users
   get 'signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   end
