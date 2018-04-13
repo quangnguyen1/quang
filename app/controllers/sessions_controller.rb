add0785e57df25317ce9198cdc5183fca07d9f91
quangnguyen1 <quangnguyen201196@gmail.com>
2018-04-13 18:16:20 +0700

    Initialize

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		log_in user
+  		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		redirect_to user 
   	else
   		flash.now[:danger] = 'Invalid email/password combination'
@@ -12,7 +13,7 @@ class SessionsController < ApplicationController
   	end
   end
   def destroy
-  	log_out
-  	redirect_to root_url
+  	log_out if logged_in?
+  	redirect_to root_url#root la duong dan tuyet doi...path la tuong doi
   end
 end
